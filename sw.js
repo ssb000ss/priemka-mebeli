e5bf6057b3f5175f74b79ce1aae19b34f796bece
dev <ssb000ss@gmail.com>
2026-09-12 02:00:56 +0500

    Приёмка мебели: офлайн-страница

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+const CACHE = 'priemka-7ef6dc2b94';
+const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+// Сначала кеш: на приёмке сеть может пропасть, а приложение должно открыться.
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request, { ignoreSearch: true })
+    .then(hit => hit || fetch(e.request)));
+});
